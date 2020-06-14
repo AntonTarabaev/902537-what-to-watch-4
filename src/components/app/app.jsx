8568a69c45e5db4b3d0dc7bf45cb6bcb586c4c67
Anton Tarabaev <aa.tarabaev@gmail.com>
2020-06-15 00:31:29 +0300

    will add prop-types

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Main from "@/components/main/main";
+import PropTypes from "prop-types";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {promoTitle, promoGenre, promoReleaseDate} = props;
+  const {promoTitle, promoGenre, promoReleaseDate, filmsTitles} = props;
 
   return (
     <Main
       promoTitle={promoTitle}
       promoGenre={promoGenre}
       promoReleaseDate={promoReleaseDate}
+      filmsTitles={filmsTitles}
     />
   );
+};
+
+App.propTypes = {
+  promoTitle: PropTypes.string.isRequired,
+  promoGenre: PropTypes.string.isRequired,
+  promoReleaseDate: PropTypes.number.isRequired,
+  filmsTitles: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
 export default App;
