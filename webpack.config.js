0a3ecd0e02abf5808b8642712f11b0f0855ad496
Anton Tarabaev <aa.tarabaev@gmail.com>
2020-06-14 14:49:52 +0300

    will set up the project configuration

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = (env) => {
+  return {
+    mode: env.type,
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, 'public')
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      open: true,
+      inline: true,
+      hot: true,
+      port: 1337,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        }
+      ],
+    },
+    devtool: env.devtool,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+      extensions: ['*', '.js', '.jsx']
+    }
+  };
+};
